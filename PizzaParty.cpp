0406d0b35f90f86f4d493197e723080f0e816300
yrshady <nehugunin@gmail.com>
2026-01-28 20:15:32 +0000

    lab 2 part 3 v2

---
+++ PizzaParty.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int heads;
+    int slices;
+    int hunger;
+
+    cout << "welcome to natanya's pizza party calculator :3" << endl;
+    cout << "how many people are coming to your party?  ";
+    cin >> heads;
+    cout << "how many slices does each pizza have?  ";
+    cin >> slices;
+    cout << "how many slices will each person eat?  ";
+    cin >> hunger;
+
+    int pizzas = heads * hunger / slices;
+    int leftovers = (pizzas * slices) - (hunger * heads);
+
+    cout << "you will need to order " << pizzas << " pizzas" <<endl;
+    cout << "you will have " << leftovers << " slices leftover" << endl;
+    cout << "party on, dude!!!!" << endl;
+
+    return 0;
+}
--- TaxCalculator.cpp
+++ TaxCalculator.cpp
@@ -12,6 +12,8 @@ int main(){
 
     double totalPrice = price + (price * taxRate);
 
+    cout << "the original price is $" << fixed << setprecision(2) << price << endl;
+    cout << "sales tax will be $" << fixed << setprecision(2) << price * taxRate <<endl;
     cout << "the price after tax is $" << fixed << setprecision(2) << totalPrice << endl;
     cout << "thank you!!!" << endl;
 
